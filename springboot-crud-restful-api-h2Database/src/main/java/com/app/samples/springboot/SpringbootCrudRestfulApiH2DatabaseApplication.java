52e43f1dd90f66044a6afc6fc2bee29dbef66e3d
abmohamm <mohd.92java@gmail.com>
2020-11-19 14:42:04 -0500

    [US1234]:Added Skeleton Project

---
+++ springboot-crud-restful-api-h2Database/src/main/java/com/app/samples/springboot/SpringbootCrudRestfulApiH2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.app.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudRestfulApiH2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestfulApiH2DatabaseApplication.class, args);
+	}
+
+}
